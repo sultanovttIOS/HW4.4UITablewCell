c41fc7538b8c42fdb1b4ce6346a865ca2f4e61eb
sultanovttIOS <alisher192377@gmail.com>
2024-01-24 19:37:14 +0600

    start hw 4.4 project

---
+++ HW4.4UITablewCell/Cell/CustomTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTableViewCell.swift
+//  HW4.4UITablewCell
+//
+//  Created by Alisher Sultanov on 24/1/24.
+//
+
+import UIKit
+import
+class CustomTableViewCell: UITableViewCell {
+
+    
+
+}
