9da400fc61b9f0dcc41f5fd3b781da265359289e
sultanovttIOS <alisher192377@gmail.com>
2024-01-24 18:49:23 +0600

    Initial Commit

---
+++ HW4.4UITablewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4.4UITablewCell
+//
+//  Created by Alisher Sultanov on 24/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
